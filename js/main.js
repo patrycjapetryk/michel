43fbdf4ab76b8b473584df6f6d73a3d28c4b3db6
Patrycja Petryk <patrycja.petryk@gmail.com>
2019-06-19 14:02:15 +0200

    semantic changes html, css, js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-const imgInContainer = document.querySelectorAll(".gallery__image");
-const howManyPhotos = imgInContainer.length;
-const header = document.querySelector(".header");
+const images = document.querySelectorAll(".gallery__image");
+const imagesLength = images.length;
+// const header = document.querySelector(".header");
 const headerTitle = document.querySelector(".header__title");
 const headerTitleText = headerTitle.innerText;
-const galleryDescription = document.querySelector(".gallery__description"); // description
-const contactContainer = document.querySelector('.contact'); // contact txt
-const linkDescription = document.querySelector('.main__link-description'); // back txt
-const bg = document.querySelector(".background"); // contact bg
+const galleryDescription = document.querySelector(".gallery__description");
+const contactArticle = document.querySelector('.contact__article');
+const contactBackground = document.querySelector(".contact__background");
+const linkDescription = document.querySelector('.main__link-description');
 
 
 const queryWidth = window.matchMedia("(max-width: 900px)");
 
-let countPhotos = 0;
-let timer;
-const time = 1000;
+let countImages = 0;
+let imagesSliderTimer;
+const imagesSliderShowTime = 1000;
 let contact = false;
 
 const imagesOnMousemove = (event) => {
@@ -22,18 +22,18 @@ const imagesOnMousemove = (event) => {
 
         const cursorX = event.clientX;
 
-        for (let i = 0; i < howManyPhotos; i++) {
+        for (let i = 0; i < imagesLength; i++) {
 
-            const partWidth = window.innerWidth / howManyPhotos;
+            const partWidth = window.innerWidth / imagesLength;
 
             const min = partWidth * i;
             const max = partWidth * (i + 1);
 
             if (cursorX >= min && cursorX < max) {
-                imgInContainer[i].style.opacity = 1;
-                galleryDescription.innerText = imgInContainer[i].getAttribute("alt");
+                images[i].style.opacity = 1;
+                galleryDescription.innerText = images[i].getAttribute("alt");
             } else {
-                imgInContainer[i].style.opacity = 0;
+                images[i].style.opacity = 0;
             }
         }
     }
@@ -41,22 +41,22 @@ const imagesOnMousemove = (event) => {
 
 const imagesSlider = () => {
     if (queryWidth.matches) {
-        for (let i = 0; i < howManyPhotos; i++) {
-            if (i == countPhotos) {
-                imgInContainer[i].style.opacity = 1;
+        for (let i = 0; i < imagesLength; i++) {
+            if (i == countImages) {
+                images[i].style.opacity = 1;
             } else {
-                imgInContainer[i].style.opacity = 0;
+                images[i].style.opacity = 0;
             }
         }
-        if (countPhotos < howManyPhotos - 1) {
-            countPhotos++;
+        if (countImages < imagesLength - 1) {
+            countImages++;
         } else {
-            countPhotos = 0;
+            countImages = 0;
         }
 
-        timer = setTimeout("imagesSlider()", time);
+        imagesSliderTimer = setTimeout("imagesSlider()", imagesSliderShowTime);
     } else {
-        clearTimeout(timer);
+        clearTimeout(imagesSliderTimer);
     }
 }
 
@@ -75,21 +75,21 @@ const backTxtPositionOut = () => {
 }
 
 const contactDisplay = (event) => {
-    const items = [headerTitle, contactContainer, linkDescription, bg];
+    const items = [headerTitle, contactArticle, linkDescription, contactBackground];
     if (contact == false) {
         // wyswietl contact
         items.forEach((item) => item.classList.add('active'));
-        contactContainer.style.display = 'block';
+        contactArticle.style.display = 'block';
         linkDescription.innerText = 'close';
-        contactContainer.addEventListener('mouseover', () => linkDescription.classList.remove('active'));
-        contactContainer.addEventListener('mouseout', () => linkDescription.classList.add('active'));
+        contactArticle.addEventListener('mouseover', () => linkDescription.classList.remove('active'));
+        contactArticle.addEventListener('mouseout', () => linkDescription.classList.add('active'));
         contact = true;
     } else {
         // ukryj contact
         headerTitle.innerText = headerTitleText;
         items.forEach((item) => item.classList.remove('active'));
 
-        setTimeout(() => contactContainer.style.display = 'none', 400);
+        setTimeout(() => contactArticle.style.display = 'none', 400);
         contact = false;
     }
 }
@@ -104,24 +104,23 @@ const init = () => {
     headerTitle.classList.remove('active');
     linkDescription.classList.remove('active');
 
-    imgInContainer[0].style.opacity = 1;
-    galleryDescription.innerText = imgInContainer[0].getAttribute("alt");
+    images[0].style.opacity = 1;
+    galleryDescription.innerText = images[0].getAttribute("alt");
 
     imagesSlider();
 
     window.addEventListener("mousemove", (event) => imagesOnMousemove(event));
 
     window.addEventListener("resize", () => {
-        clearTimeout(timer);
+        clearTimeout(imagesSliderTimer);
         imagesSlider();
     });
 
     // title + contact page
-
     headerTitle.addEventListener('mouseover', () => titleHover("Contact"));
     headerTitle.addEventListener('mouseout', () => titleHover(headerTitleText));
     headerTitle.addEventListener('click', contactDisplay);
-    bg.addEventListener('click', contactDisplay);
+    contactBackground.addEventListener('click', contactDisplay);
 }
 
 if (!queryWidth.matches) {
